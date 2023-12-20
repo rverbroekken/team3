24173f2f29bcee9bbc5c070cdc0f1c33fb5d9344
ROEL\roel <roel@storygiantgames.com>
2023-12-20 23:12:42 +0100

    Gui items

---
+++ Assets/Scripts/Dialogs/PlayResult.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayResult : MonoBehaviour
+{
+    public Image m_Background;
+    public Button m_ContinueButton;
+    public RectTransform m_Star1;
+    public RectTransform m_Star2;
+    public RectTransform m_Star3;
+    public TextMeshProUGUI m_Header;
+    public TextMeshProUGUI m_Description;
+
+    public void OnContinue()
+    {
+
+    }
+}
+
+
