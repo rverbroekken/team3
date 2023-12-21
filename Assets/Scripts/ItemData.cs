2b29b8f892bb71deced7bc77bbccb36464206d28
ROEL\roel <roel@storygiantgames.com>
2023-12-21 13:08:18 +0100

    higher rotation possible

--- Assets/Scripts/ItemData.cs
+++ Assets/Scripts/ItemData.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 public class ItemData : ScriptableObject
 {
     [Header("Rotation Settings")]
-    [Range(-2f, 2f)] public float minXRotation = -1f;
-    [Range(-2f, 2f)] public float minYRotation = -1f;
-    [Range(-2f, 2f)] public float minZRotation = -1f;
-    [Range(-2f, 2f)] public float maxXRotation = 1f;
-    [Range(-2f, 2f)] public float maxYRotation = 1f;
-    [Range(-2f, 2f)] public float maxZRotation = 1f;
+    [Range(-10f, 10f)] public float minXRotation = -1f;
+    [Range(-10f, 10f)] public float minYRotation = -1f;
+    [Range(-10f, 10f)] public float minZRotation = -1f;
+    [Range(-10f, 10f)] public float maxXRotation = 1f;
+    [Range(-10f, 10f)] public float maxYRotation = 1f;
+    [Range(-10f, 10f)] public float maxZRotation = 1f;
 
     [Header("Force Settings")]
     [Range(15f, 23f)] public float minForce = 16f;
